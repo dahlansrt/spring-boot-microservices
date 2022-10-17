7da9712894dd672a7cc67240d269dc16c7ea4492
dahlan <dahlansrt@gmail.com>
2022-10-17 16:33:43 +0700

    Service Discovery
    - Add service discovery server
    - Configure eureka client on each service

--- order-service/src/main/java/id/faroga/orderservice/service/OrderService.java
+++ order-service/src/main/java/id/faroga/orderservice/service/OrderService.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
     public void placeOrder(OrderRequest orderRequest){
        Order order= new Order();
        order.setOrderNumber(UUID.randomUUID().toString());
@@ -37,8 +37,8 @@ public class OrderService {
                 .toList();
 
         // call Inventory service & place order if product is in stock
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8083/api/inventory",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
